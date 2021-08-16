3ae25f685364b2c2bc7d6dd2c1b75e1259653637
luisglm <69292415+luisglm@users.noreply.github.com>
2021-08-15 22:47:36 -0500

    p1

--- Greedy/TSP/greddy.py
+++ Greedy/TSP/greddy.py
@@ -1,1 +1,14 @@
-print("greddy")
+
+
+from random import random
+from random import randint
+
+A = 10
+max = 250
+X=[]
+
+for i in range(A):
+    x=randint(0,max)
+    X.append(x)
+    
+print(X)    
